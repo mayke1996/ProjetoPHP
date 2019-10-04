22bf5c0521c4987b19476685632ae13695fc5a71
Mayke Ferreira Mônica - 118110015 <118110015@animaedu.intranet>
2019-10-04 20:27:49 -0300

    Primeiro arquivo php

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+define("IP", "127.0.0.1");
+echo IP;
+
+echo "<br>";
+$numero = 1;
+	echo $numero;
+
+echo "<br>";
+echo "Hello World" ;
+echo"<br>";
+
+$num1 = 1;
+$num2 = 2;
+
+echo("O Resultado da soma é: " .($num1 + $num2));
+echo"<br>";
+
+<?php
+
+define("IP", "127.0.0.1");
+echo IP;
+
+echo "<br>";
+$numero = 1;
+	echo $numero;
+
+echo "<br>";
+echo "Hello World" ;
+echo"<br>";
+
+$num1 = 1;
+$num2 = 2;
+
+echo("O Resultado da soma é: " .($num1 + $num2));
+echo"<br>";
+
